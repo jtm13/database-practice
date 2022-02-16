9e6f963c8a23d083687874e284c6a202670ee026
jtm13 <74789040+jtm13@users.noreply.github.com>
2022-02-16 15:52:33 -0500

    Had some things I did not want to commit

---
+++ src/container.php
@@ -0,0 +1,22 @@
+<?php 
+define("NO_PICTURE", "placeholder");
+abstract class Container {
+    private $ar;
+    function __construct($it) {
+        $this->ar = $it;
+    }
+
+    protected function getArray() {
+        return $this->ar;
+    }
+
+    protected function checkPicture(string $source): string {
+        if (strcmp($source, "no img") !== 0) {
+            return $source;
+        }
+        return NO_PICTURE;
+    }
+          
+    abstract function info(): string;
+}
+?>
